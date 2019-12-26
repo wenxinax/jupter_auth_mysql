7012adf089b23a60b5a40e783b38040b1b4ab1f6
wenxinax <08163279@cumt.edu.cn>
2019-12-26 15:50:49 +0800

    init

---
+++ mysqlauthenticator/mysqlauthenticator.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+
+from jupyterhub.auth import Authenticator
+from tornado import gen
+
+from mysqlauthenticator.DAO.base import init
+from mysqlauthenticator.DAO.user import User
+
+
+class MysqlAuthenticator(Authenticator):
+
+	"""JupyterHub Authenticator Based on Mysql"""
+
+	def __init__(self, **kwargs):
+		super(MysqlAuthenticator, self).__init__(**kwargs)
+
+	@gen.coroutine
+	def authenticate(self, handler, data):
+
+		db_url = "mysql+mysqlconnector://root:<root>@<192.168.199.182>:3306/<jupyter>"
+		session = init(db_url)
+
+		username = data['username']
+		passwd = data['password']
+
+		try:
+			user = session.query(User).filter(User.username == username).one()
+			if user and user.check_password(passwd):
+				return user.username
+			else:
+				return None
+		except:
+			return None
+
+
